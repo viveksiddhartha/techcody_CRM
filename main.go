b17c480c7f0241837e3fae2ff9840a40593636ef
Cody <siddelhi1@gmail.com>
2019-08-05 17:53:36 +0530

    Update main.go
    updated code

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	router.HandleFunc("/", handlers.HomeHandler).Methods("GET")
 	router.Handle("/entity", handlers.EntityCreate(&env))
 	router.Handle("/profile", handlers.ProfileCreate(&env))
-	router.Handle("/searchprofile", handlers.UpdateProfile(&env))
+	router.Handle("/updateprofile", handlers.UpdateProfile(&env))
 
 	//Lister defined for end point
 	/*
